09abcded6db23802a7dfb5b852e8ccc46427bb6f
Tung Nguyen <tung.berlin@gmail.com>
2011-05-02 16:55:16 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chapper3App::Application.config.secret_token = '46862a52da7cfda336c47c65f1c4a11d377a5d1e4f9fb789080b6d8c278f44be1ed1c6b35871709030d418160637a8f2bcedb1abca4f3eb440e027c6692f8ec2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Chapper3App::Application.config.session_store :cookie_store, :key => '_chapper3_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Chapper3App::Application.config.session_store :active_record_store
